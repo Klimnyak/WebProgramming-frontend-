eb9648289ea575b699554b674bbaf71e7aaaf9c8
Klimnyak <ppklim04@gmail.com>
2024-10-23 14:14:10 +0300

    lab6 and 7

---
+++ lab6/webpack-project/src/index.js
@@ -0,0 +1,5 @@
+import './scss/rozklad-style.scss'; // Шлях до вашого CSS-файлу
+import './scss/news-style.scss';
+import './scss/img-style.scss';
+import './scss/styles.scss';
+console.log("Hello world!"); 
---
+++ lab7/webpack-project/src/index.js
@@ -0,0 +1,6 @@
+import './scss/rozklad-style.scss'; // Шлях до вашого CSS-файлу
+import './scss/news-style.scss';
+import './scss/img-style.scss';
+import './scss/styles.scss';
+import './scss/print.scss';
+console.log("Hello world!"); 
